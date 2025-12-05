bad90840a64a32b627688f75c2bec0aac6b5f7b5
jason <jason@noisybox.net>
2025-12-05 11:44:49 -0800

    fresh

---
+++ day05/fresh.py
@@ -0,0 +1,52 @@
+from sys import stdin
+
+def read_ranges():
+    ranges = list()
+    for line in stdin:
+        if len(line.strip()) == 0:
+            return ranges
+        range = line.strip().split('-')
+        range = list(map(int, range))
+        ranges.append(range)
+    return ranges
+
+def optimize(ranges):
+    s = sorted(ranges, key = lambda x: x[0])
+    i = 0
+    term = len(s)-1
+    while i < term:
+        r = s[i]
+        n = s[i+1]
+        if r[1] >= n[0]:
+            print("combine {0} and {1}".format(r, s[i+1]))
+            s.pop(i)
+            s.pop(i)
+            s.insert(i, [r[0], max(r[1], n[1])])
+            term = len(s)-1
+        else:
+            i = i + 1
+    return s
+
+def is_fresh(id, ranges):
+    for r in ranges:
+        # print("check {0} {1} {2}".format(id, r[0], r[1]))
+        if id >= r[0] and id <= r[1]:
+            return (True,r)
+    return (False,[])
+
+original_ranges = read_ranges()
+print(original_ranges)
+
+ranges = optimize(original_ranges)
+print(ranges)
+
+ct = 0
+for line in stdin:
+    id = int(line.strip())
+    (fresh,range) = is_fresh(id, ranges)
+    print("{0} => fresh? {1} {2}".format(id, fresh, range))
+    if fresh:
+        ct = ct + 1
+
+print("---------")
+print(ct)
